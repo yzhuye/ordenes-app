195193478f5497925532862cd7c0660dc5f0a2ec
jesusckantillo <jesusguerrrero@uninorte.edu.co>
2025-08-18 17:39:20 -0500

    feat: add swagger for docs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,34 @@
+// src/main.ts
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // CORS
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    credentials: true,
+  });
+
+  // Validación global
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  // Swagger (debe ir DESPUÉS de crear app)
+  const config = new DocumentBuilder()
+    .setTitle('Orders API')
+    .setDescription('Endpoints del módulo de Órdenes')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: { persistAuthorization: true },
+  });
+
   await app.listen(process.env.PORT || 3000);
+  console.log('Docs -> http://localhost:3000/docs');
 }
 bootstrap();
